8e4fe7257ac72e7e98dcd0ced6b86a9d57d3618e
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-10-06 03:57:34 +0800

    hello world!

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Home Manager Configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      mkHomeConfiguration =
+        {
+          system,
+          username ? "ycg",
+        }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          extra = {
+            inherit username;
+          };
+        in
+
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # use extraSpecialArgs to pass through arguments to home-manager modules
+          extraSpecialArgs = {
+            inherit system extra;
+          };
+
+          modules = [
+            ./modules/general.nix
+            ./modules/utils.nix
+          ];
+        };
+    in
+    {
+      homeConfigurations.orbstack = mkHomeConfiguration {
+        system = "aarch64-linux";
+      };
+    };
+}
---
+++ modules/general.nix
@@ -0,0 +1,8 @@
+{ extra, ... }:
+{
+  home.username = extra.username;
+  home.homeDirectory = "/home/${extra.username}";
+  # Check the release notes carefully before updating the state version
+  # https://nix-community.github.io/home-manager/release-notes.xhtml
+  home.stateVersion = "25.11";
+}
