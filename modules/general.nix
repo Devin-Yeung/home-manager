eaf1b39488ae27338ad45e1c362a3019949b0ee1
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-10-06 04:37:43 +0800

    feat: let home-manager manage itself

--- modules/general.nix
+++ modules/general.nix
@@ -2,7 +2,11 @@
 {
   home.username = extra.username;
   home.homeDirectory = "/home/${extra.username}";
+
   # Check the release notes carefully before updating the state version
   # https://nix-community.github.io/home-manager/release-notes.xhtml
   home.stateVersion = "25.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
